c17773249c539b101c6060ffcc98f45c40a593cc
eloyou <angelobcaringal@gmail.com>
2024-01-10 06:12:20 +0800

    Added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        //ask the user for thier name
         Console.Write("What is your Firstname? ");
         string firstname = Console.ReadLine();
 
-        Console.WriteLine($"Hello {firstname}");
+        Console.Write("What is your Lastname? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Hello, You're  {firstname} {lastname} thank you for coming!");
     }
 }

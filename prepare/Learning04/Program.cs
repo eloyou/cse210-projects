53bd36c19198d4747c0d592d1004a681a5053bcb
eloyou <angelobcaringal@gmail.com>
2024-01-29 22:34:53 +0800

    final output

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Angelo", "Programming");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Paula Putong", "Accounting", "4.5", "6-9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        Writing a3 = new Writing("Isaac Caringal", "Baby", "Our first born child");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/writing.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Writing : Assignment
+{
+    private string _title;
+
+    public Writing(string studentName, string topic, string title) : base(studentName,topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        return $"{_title}";
+    }
+}
